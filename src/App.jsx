c547c6fff8f197911ef9e1b432dee020d10087e8
Andy Blackledge <blackledge22@gmail.com>
2020-03-22 15:53:40 +0000

    Initial commit with modified CRA

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={`app ${styles.testing}`}>
+      <p>EE Calculator</p>
+    </div>
+  );
+}
+
+export default App;
